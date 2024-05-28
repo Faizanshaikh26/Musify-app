12c451862da50a80e738629b990fbcafab96c446
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-05-28 08:47:12 +0530

    realc1

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,30 @@
-import React, { useState } from "react";
+import React, { Suspense, lazy } from 'react';
+import Sidebar from './Components/Sidebar';
+import './App.css';
+import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
+import Player from './Components/Player';
+import { usePlayer } from './Context/PlayerContext';
+
+// Lazy load the Displayalbums component
+const Displayalbums = lazy(() => import('./albums/Displayalbums'));
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const getData = async () => {
-    try {
-      const response = await fetch("https://musify-rest-api.onrender.com/", {
-        method: "GET",
-        headers: {},
-      });
-
-      if (response.ok) {
-        const result = await response.json();
-        setData(result.albums);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  const { audioRef } = usePlayer();
 
   return (
-    <>
-      <button className="" onClick={getData}>
-        Get Data
-      </button>
-
-      {data.map((album) => (
-        <div key={album._id}>
-          <h1>{album.title}</h1>
-          {album.songs.map((song) => (
-            <div key={song._id}>
-              <p>{song.title}</p>
-              <img src={song.songImage} alt={song.title} />
-              <p>{song.songImage}</p>
-              <audio controls>
-                <source src={song.songUrl} type="audio/mpeg" />
-                Your browser does not support the audio element.
-              </audio>
-              {/* Add more song details here */}
-            </div>
-          ))}
-        </div>
-      ))}
-    </>
+    <div className='bg-black text-white h-full'>
+      <Sidebar>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/album/:id' element={<Displayalbums />} />
+          </Routes>
+        </Suspense>
+        <Player />
+      </Sidebar>
+      <audio ref={audioRef} preload='auto' />
+    </div>
   );
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Slider from './Components/Slider'
+import Navbar from './Components/Navbar'
+import Loki from './Components/Loki'
+import DisplayHome from './Components/DisplayHome'
+
+import Footer from './Components/Footer'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+     <div className='w-[100%] mt-1 px-2 rounded bg-[#212020] text-white overflow-auto '>
+   
+   <Navbar/>
+    <Slider/>
+{/* <Loki/> */}
+   <DisplayHome/>
+   <Footer/>
+
+
+
+     </div>
+    </>
   )
 }
 
