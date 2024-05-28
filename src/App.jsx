c34b3068cf8d9f7561c75790ec7601747cd2629b
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-05-28 08:45:01 +0530

    testll

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import React, { Suspense, lazy } from 'react';
-import Sidebar from './Components/Sidebar';
-import './App.css';
-import Home from './Home';
-import { Route, Routes } from 'react-router-dom';
-import Player from './Components/Player';
-import { usePlayer } from './Context/PlayerContext';
-
-// Lazy load the Displayalbums component
-const Displayalbums = lazy(() => import('./albums/Displayalbums'));
+import React, { useState } from "react";
 
 function App() {
-  const { audioRef } = usePlayer();
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch("https://musify-rest-api.onrender.com/", {
+        method: "GET",
+        headers: {},
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setData(result.albums);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-    <div className='bg-black text-white h-full'>
-      <Sidebar>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/album/:id' element={<Displayalbums />} />
-          </Routes>
-        </Suspense>
-        <Player />
-      </Sidebar>
-      <audio ref={audioRef} preload='auto' />
-    </div>
+    <>
+      <button className="" onClick={getData}>
+        Get Data
+      </button>
+
+      {data.map((album) => (
+        <div key={album._id}>
+          <h1>{album.title}</h1>
+          {album.songs.map((song) => (
+            <div key={song._id}>
+              <p>{song.title}</p>
+              <img src={song.songImage} alt={song.title} />
+              <p>{song.songImage}</p>
+              <audio controls>
+                <source src={song.songUrl} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+              {/* Add more song details here */}
+            </div>
+          ))}
+        </div>
+      ))}
+    </>
   );
 }
 
